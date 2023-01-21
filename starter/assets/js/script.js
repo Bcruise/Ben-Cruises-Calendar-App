fbf22df9a8c11f9203f8ac642ee64060a8ee341a
Ben Cruise <ben_cruise@hotmail.co.uk>
2023-01-21 19:06:59 +0000

    first commit

---
+++ starter/assets/js/script.js
@@ -0,0 +1,100 @@
+// Set Date
+
+var today = moment();
+
+$('#currentDay').text(today.format('dddd, MMMM Do'));
+
+
+// Main Array
+
+if (localStorage.getItem('mainArray') == null) {
+    var mainArray = 
+ [{
+    time: "9AM",
+    timeToTest: 9,
+    innerText: "",
+},
+{
+    time: "10AM",
+    timeToTest: 10,
+    innerText: "",
+},
+{
+    time: "11AM",
+    timeToTest: 11,
+    innerText: "",
+},
+{
+    time: "12PM",
+    timeToTest: 12,
+    innerText: "",
+},
+{
+    time: "1PM",
+    timeToTest: 13,
+    innerText: "",
+},
+{
+    time: "2PM",
+    timeToTest: 14,
+    innerText: "",
+},
+{
+    time: "3PM",
+    timeToTest: 15,
+    innerText: "",
+},
+{   
+    time: "4PM",
+    timeToTest: 16,
+    innerText: "",
+},
+{   
+    time: "5PM",
+    timeToTest: 17,
+    innerText: "",
+}];
+
+} else {
+    mainArray = JSON.parse(localStorage.getItem('mainArray'));
+}
+
+// function
+
+for (var a = 0; a < mainArray.length; a++) {
+    var eachHour = $('<div>');
+    eachHour.addClass("col-12 eachHour");
+    eachHour.attr('id', 'eachHour');
+
+    var timeSection = $('<div>');
+    timeSection.addClass("col-1 timeSection");
+    timeSection.text(mainArray[a].time);
+
+    var textArea = $('<textarea>');
+    
+    var hourNow = moment().format('HH');
+    
+    if ((hourNow - mainArray[a].timeToTest) < 0) {
+        textArea.addClass("col-10 textArea green");
+    } else if ((hourNow - mainArray[a].timeToTest) == 0) {
+        textArea.addClass("col-10 textArea red");
+    } else {
+        textArea.addClass("col-10 textArea grey");
+    }
+
+    var saveButton = $('<button>');
+    saveButton.addClass("col-1 saveButton");
+
+    var faSaveIcon = $('<i></i>');   
+    faSaveIcon.addClass("far fa-save fa-lg");
+
+    var container = $('.container');
+    container.append(eachHour);
+    eachHour.append(timeSection);
+    eachHour.append(textArea);
+    eachHour.append(saveButton);
+    saveButton.append(faSaveIcon);
+
+    localStorage.setItem('mainArray', JSON.stringify(mainArray));
+}
+
